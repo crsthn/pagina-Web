1f8e6b05a42b3c69c3206eb888a32017d23cc8f9
Cristhian Guzman <cristhianguzman93@gmail.com>
2018-12-12 14:48:36 -0500

    Initial commit

---
+++ php/register.php
@@ -0,0 +1,19 @@
+<?php 
+
+$name=$_POST["name"];
+$lastName=$_POST["lastName"];
+$username=$_POST["username"];
+$email=$_POST["email"];
+$password=$_POST["password"];
+$confirmPassword=$_POST["confirmPassword"];
+
+echo "Hola: ".$name;
+
+
+
+
+
+
+
+
+ ?>
